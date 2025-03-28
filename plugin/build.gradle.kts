2ac2e6605f599c0fd08b55668270ea3428ac0722
diegonighty <cardenasdiego003@gmail.com>
2025-03-28 14:23:15 -0500

    feat: add optional dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -24,12 +24,14 @@ paperPluginYaml {
   description = "Example description."
   dependencies {
     server("VanguardCore", PaperPluginYaml.Load.BEFORE, true, joinClasspath = true)
+    //server("VanguardEssentials", PaperPluginYaml.Load.BEFORE, true, joinClasspath = true) // Uncomment this line if you want to add essentials as dependency
   }
 }
 
 dependencies {
   //paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT") // Uncomment this line if you want to use Paperweight
   //compileOnly(fileTree(mapOf("dir" to "../libs", "include" to listOf("*.jar")))) // Uncomment this line if you want to use .jar libs
+  //compileOnly("com.vanguardfactions:essentials-plugin:0.0.1") // Uncomment this line if you want to add essentials as dependency (change version)
   compileOnlyApi(libs.paper)
   compileOnlyApi(libs.core)
   annotationProcessor(libs.core)
@@ -40,6 +42,11 @@ tasks {
     val projectName = StringUtils.capitalise(project.name)
     archiveFileName.set("Vanguard${projectName}-${project.version}.jar")
   }
+/*
+  reobfJar {
+    outputJar = layout.buildDirectory.file("libs/Vanguard${projectName}-${project.version}-reobfJar.jar")
+  }
+ */
 }
 
 
